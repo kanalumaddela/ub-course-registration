e547518c75414f3d6030550859a99890ef9b0a78
kanalumaddela <git@maddela.org>
2021-05-01 05:22:19 -0400

    quick way to check and force https
    Signed-off-by: kanalumaddela <git@maddela.org>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,8 +23,17 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Request $request)
     {
-        //
+        $isSecure = $request->server('HTTPS', 'off') !== 'off' || $request->server('SERVER_PORT') == 443 || $request->server('HTTP_X_FORWARDED_PROTO') === 'https';
+
+        if ($isSecure) {
+            URL::forceScheme('https');
+        }
+    }
+
+    public static function isSecure()
+    {
+
     }
 }
